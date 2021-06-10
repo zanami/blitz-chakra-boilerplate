df0a970331289e85bef399014ed1446faf9f3b25
zanami <zanami@users.noreply.github.com>
2021-06-10 14:22:22 +1000

    Initial commit

---
+++ app/pages/_app.tsx
@@ -0,0 +1,73 @@
+import { ChakraProvider } from "@chakra-ui/react"
+import LoginForm from "app/auth/components/LoginForm"
+import { APP_NAME } from "app/config"
+import theme from "app/core/theme"
+import {
+  AppProps,
+  AuthenticationError,
+  AuthorizationError,
+  ErrorComponent,
+  ErrorFallbackProps,
+  Head,
+  useQueryErrorResetBoundary,
+  useRouter,
+} from "blitz"
+import { DefaultSeo } from "next-seo"
+import React, { Suspense } from "react"
+import { ErrorBoundary } from "react-error-boundary"
+
+export default function App({ Component, pageProps }: AppProps) {
+  const getLayout = Component.getLayout || ((page) => page)
+  const router = useRouter()
+
+  return (
+    <Suspense fallback="Loading...">
+      <DefaultSeo defaultTitle={`${APP_NAME}`} titleTemplate={`%s | ${APP_NAME}`} />
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1, viewport-fit=cover, user-scalable=no" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#6236ff" />
+        <meta name="msapplication-TileImage" content="/mstile-144x144.png" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <ChakraProvider theme={theme}>
+        <Suspense fallback="Loading...">
+          <ErrorBoundary
+            FallbackComponent={RootErrorFallback}
+            resetKeys={[router.asPath]}
+            onReset={useQueryErrorResetBoundary().reset}
+          >
+            {getLayout(<Component {...pageProps} />)}
+          </ErrorBoundary>
+        </Suspense>
+      </ChakraProvider>
+    </Suspense>
+  )
+}
+
+function RootErrorFallback({ error, resetErrorBoundary }: ErrorFallbackProps) {
+  if (error instanceof AuthenticationError) {
+    return <LoginForm onSuccess={resetErrorBoundary} />
+  } else if (error instanceof AuthorizationError) {
+    return (
+      <ErrorComponent
+        statusCode={error.statusCode}
+        title="Sorry, you are not authorized to access this"
+      />
+    )
+  } else {
+    return (
+      <ErrorComponent statusCode={error.statusCode || 400} title={error.message || error.name} />
+    )
+  }
+}
